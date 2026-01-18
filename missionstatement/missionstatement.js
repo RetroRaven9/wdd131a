42abc478619ec78ca6a1ec27b11e383bddd12072
Tylar Hill <tylarbh@byui.edu>
2026-01-17 17:32:09 -0700

    logo

--- missionstatement/missionstatement.js
+++ missionstatement/missionstatement.js
@@ -8,8 +8,14 @@ function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
         // code for changes to colors and logo
+        document.body.style.backgroundColor = '#333';
+        document.body.style.color = '#fff';
+        logo.src = 'byui-logo-black.png'; // example logo for dark mode
     } else {
         // code for changes to colors and logo
+        document.body.style.backgroundColor = '#fff';
+        document.body.style.color = '#000';
+        logo.src = 'byui-logo-blue.webp'; // example logo for light mode
     }
 }           
                     
